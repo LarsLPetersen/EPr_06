9c263abc945c9c574f6bf8b07efc297bd23d7411
Lilian Mendoza de Sudan <lilian_mendoza@hotmail.com>
2016-12-27 16:22:28 +0100

    python test
    Ich habe hier die .py gestartet… zum Testen

---
+++ start.py
@@ -0,0 +1,11 @@
+"""This programm will simulate an hotel reception."""
+
+__author__ = "5625448: Lilian Mendoza de Sudan, 6xxxxxx: Lars Petersen"
+__copyright__ = "Copyright 2016/2017 – EPR1-Goethe-Universitaet" 
+__email__ = "lilian_mendoza@hotmail.com, petersen@informatik.uni-frankfurt.de" 
+
+
+class Guest(object):
+    """Create an hotel guest"""
+
+    def __init__(self, guest_id, wlan_code, 
